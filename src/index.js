2dd8da9587dadfc887cab9ef0ec7b0ece82d4b22
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-10-16 00:54:12 +0300

    initial settings commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// src/index.js 
+
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.json({
+		message: 'Hello world!',
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`);
+});
+
