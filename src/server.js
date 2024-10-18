32d858fccd42eb4615c57325f053d97e20039a4a
YuliaShylina <yuliyashylina@gmail.com>
2024-10-18 10:04:01 +0300

    sawgger00

--- src/server.js
+++ src/server.js
@@ -1,45 +1,46 @@
-// src/server.js
-
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = 3000;
 
 export const startServer = () => {
-	const app = express();
+  const app = express();
 
-	app.use(express.json());
-	app.use(cors());
+  app.use(express.json());
+  app.use(cors());
 
-	app.use(
-		pino({
-			transport: {
-				target: 'pino-pretty',
-			},
-		}),
-	);
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-	app.get('/', (req, res) => {
-		res.json({
-			message: 'Hello world!',
-		});
-	});
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
 
-	app.use('*', (req, res, next) => {
-		res.status(404).json({
-			message: 'Not found',
-		});
-	});
+  app.use('/api-docs', swaggerDocs());
 
-	app.use((err, req, res, next) => {
-		res.status(500).json({
-			message: 'Something went wrong',
-			error: err.message,
-		});
-	});
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
 
-	app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}`);
-	});
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
