a8ec1899b82ed4d8299ed1b2c83d50a47bc737a2
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-10-19 16:08:50 +0300

    fix MW authenticate and user router

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,8 +1,10 @@
 import createHttpError from "http-errors";
 
 import * as authServices from "../services/auth.js";
+import * as userServices from '../services/users.js';
 
 const authenticate = async (req, res, next) => {
+	console.log("hello");
 	// const {authorization} = req.headers;
 	const authorization = req.get("Authorization");
 
@@ -17,6 +19,7 @@ const authenticate = async (req, res, next) => {
 	}
 
 	const session = await authServices.findSessionByAccessToken(token);
+
 	if (!session) {
 		return next(createHttpError(401, "Session not found"));
 	}
@@ -24,13 +27,16 @@ const authenticate = async (req, res, next) => {
 	if (new Date() > session.accessTokenValidUntil) {
 		return next(createHttpError(401, "Access token expired"));
 	}
+	console.log(session);
 
-	const user = await authServices.findUser({ _id: session.userId });
+	const user = await userServices.getUser(session.userId);
 	if (!user) {
 		return next(createHttpError(401, "User not found"));
 	}
 
 	req.user = user;
+	console.log(user);
+
 
 	next();
 };
