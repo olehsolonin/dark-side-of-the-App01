2da0adf54cba52a6c33586fe6218c1f3eaa86623
YuliaShylina <yuliyashylina@gmail.com>
2024-10-20 13:38:44 +0300

    upDateUserSchema

--- src/db/models/users.js
+++ src/db/models/users.js
@@ -1,53 +1,58 @@
-// src/db/models/student.js 
+// src/db/models/student.js
 
 import { model, Schema } from 'mongoose';
-import { handleSaveError, setUpdateOptions } from "./hooks.js";
+import { handleSaveError, setUpdateOptions } from './hooks.js';
 
 const usersSchema = new Schema(
-	{
-		name: {
-			type: String,
-			required: false,
-			default: 'user'
-		},
-		dailyNorm: {
-			type: Number,
-			required: false,
-			default: 1800
-		},
-		gender: {
-			type: String,
-			enum: ['Man', 'Woman', 'transformer'],
-			default: 'Woman',
-			required: false
-		},
-		email: {
-			type: String,
-			match: [/\S+@\S+\.\S+/, 'Please enter a valid email'],
-			unique: true,
-			required: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		photo: {
-			type: String,
-			required: false,
-			default: ''
-		}
-	},
-	{
-		timestamps: true,
-		versionKey: false,
-	},
+  {
+    name: {
+      type: String,
+      required: false,
+      default: 'user',
+    },
+    dailyNorm: {
+      type: Number,
+      required: false,
+      default: 1800,
+    },
+    dailyWaterIntake: {
+      type: Number,
+      required: false,
+      default: 500,
+    },
+    gender: {
+      type: String,
+      enum: ['Man', 'Woman', 'transformer'],
+      default: 'Woman',
+      required: false,
+    },
+    email: {
+      type: String,
+      match: [/\S+@\S+\.\S+/, 'Please enter a valid email'],
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
-usersSchema.post("save", handleSaveError);
+usersSchema.post('save', handleSaveError);
 
-usersSchema.pre("findOneAndUpdate", setUpdateOptions);
+usersSchema.pre('findOneAndUpdate', setUpdateOptions);
 
-usersSchema.post("findOneAndUpdate", handleSaveError);
+usersSchema.post('findOneAndUpdate', handleSaveError);
 
 export const UsersCollection = model('user', usersSchema);
 
