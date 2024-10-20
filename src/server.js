64384262b4abcdf0e9b3c71089a466d322e40af3
YuliaShylina <yuliyashylina@gmail.com>
2024-10-20 17:38:40 +0300

    addSwaggerDocs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 // src/server.js
-
+import cookieParser from 'cookie-parser';
 import express from 'express';
 // import pino from 'pino-http';
 import cors from 'cors';
+import { env } from './utils/env.js';
+import { UPLOAD_DIR } from './constants/index.js';
+import authRouter from './routers/auth.js';
+import waterRouter from './routers/water.js';
+import userRouter from './routers/users.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -11,20 +16,20 @@ export const startServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
+  // app.use(
+  // 	pino({
+  // 		transport: {
+  // 			target: 'pino-pretty',
+  // 		},
+  // 	}),
+  // );
+  app.use('/auth', authRouter);
+  app.use('/users', userRouter);
+  app.use('/water', waterRouter);
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
+  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
