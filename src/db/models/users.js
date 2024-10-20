5df40fd754417a9ca3dc35abfff173b36ab0edfa
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-10-20 15:10:53 +0300

    fix transformer gender

--- src/db/models/users.js
+++ src/db/models/users.js
@@ -4,48 +4,48 @@ import { model, Schema } from 'mongoose';
 import { handleSaveError, setUpdateOptions } from './hooks.js';
 
 const usersSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: false,
-      default: 'user',
-    },
-    dailyNorm: {
-      type: Number,
-      required: false,
-      default: 1800,
-    },
-    dailyWaterIntake: {
-      type: Number,
-      required: false,
-      default: 500,
-    },
-    gender: {
-      type: String,
-      enum: ['Man', 'Woman', 'transformer'],
-      default: 'Woman',
-      required: false,
-    },
-    email: {
-      type: String,
-      match: [/\S+@\S+\.\S+/, 'Please enter a valid email'],
-      unique: true,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    photo: {
-      type: String,
-      required: false,
-      default: '',
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+	{
+		name: {
+			type: String,
+			required: false,
+			default: 'user',
+		},
+		dailyNorm: {
+			type: Number,
+			required: false,
+			default: 1800,
+		},
+		dailyWaterIntake: {
+			type: Number,
+			required: false,
+			default: 500,
+		},
+		gender: {
+			type: String,
+			enum: ['Man', 'Woman'],
+			default: 'Woman',
+			required: false,
+		},
+		email: {
+			type: String,
+			match: [/\S+@\S+\.\S+/, 'Please enter a valid email'],
+			unique: true,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		photo: {
+			type: String,
+			required: false,
+			default: '',
+		},
+	},
+	{
+		timestamps: true,
+		versionKey: false,
+	},
 );
 
 usersSchema.post('save', handleSaveError);
