0ea5e061cd932ac5a0b65c9c54c2fa2124512261
YuliaShylina <yuliyashylina@gmail.com>
2024-10-25 10:22:19 +0300

    chagedswaggerAuthUsers

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
 import { upload } from '../middlewares/multer.js';
 import {
-	getUserInfoController,
-	addUserPhotoController,
-	patchUserController,
+  getUserInfoController,
+  addUserPhotoController,
+  patchUserController,
 } from '../controllers/user.js';
 import { updateUserSchema } from '../validation/user.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
@@ -19,17 +19,17 @@ userRouter.get('/', ctrlWrapper(getUserInfoController));
 
 // Додати/змінити фото користувача
 userRouter.post(
-	'/photo',
-	upload.single('photo'),
-	ctrlWrapper(addUserPhotoController),
+  '/photo',
+  upload.single('photo'),
+  ctrlWrapper(addUserPhotoController),
 );
 
 // Оновити інформацію про користувача
-userRouter.patch(
-	'/:userID',
-	//   jsonParser,
-	validateBody(updateUserSchema),
-	ctrlWrapper(patchUserController),
+userRouter.post(
+  '/',
+  //   jsonParser,
+  validateBody(updateUserSchema),
+  ctrlWrapper(patchUserController),
 );
 
 export default userRouter;
