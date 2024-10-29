72e7dca3349427035f9a1458262561a4ea4c7b19
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-10-29 10:21:16 +0200

    fix Joi schema bugs

--- src/validation/user.js
+++ src/validation/user.js
@@ -5,10 +5,10 @@ export const updateUserSchema = Joi.object({
 	email: Joi.string().email(),
 	password: Joi.string().min(8).max(64),
 	gender: Joi.string().valid('Man', 'Woman'),
-	dailyNorm: Joi.string(),
-	dailyWaterIntake: Joi.string(),
+	dailyNorm: Joi.number(),
+	dailyWaterIntake: Joi.number(),
 });
 
 export const updateDailyNormSchema = Joi.object({
-	dailyNorm: Joi.string().required(),
+	dailyNorm: Joi.number().required(),
 });
